5ec20404f238413b1cc06787b160b7ab1f118290
Prashant Verma SDE <114382166+sohit17412@users.noreply.github.com>
2024-09-27 17:19:15 +0530

    Add files via upload

---
+++ Card slider/script.js
@@ -0,0 +1,30 @@
+﻿new Swiper('.card-wrapper', {
+
+
+    spaceBetween:30,
+    
+    loop: true,
+    
+    
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView:1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
